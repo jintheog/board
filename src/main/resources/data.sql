904965331347b38c93a9c9fe485b93210086a9af
stanstanstanlee <stanstanstanlee@gmail.com>
2025-11-24 09:26:54 +0900

    테이블 생성, 데이터 추가 sql 파일 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO post (title, content) values ('첫번째 게시물', 'hello');
+INSERT INTO post (title, content) values ('두번째 게시물', 'hi');
+INSERT INTO post (title, content) values ('세번째 게시물', 'bye');
