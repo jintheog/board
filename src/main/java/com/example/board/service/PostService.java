b49b3cec1b23748906e5e7acd40243f469d2256a
stanstanstanlee <stanstanstanlee@gmail.com>
2025-11-25 11:16:54 +0900

    최신 게시물 출력 기능 구현

--- src/main/java/com/example/board/service/PostService.java
+++ src/main/java/com/example/board/service/PostService.java
@@ -3,6 +3,7 @@ package com.example.board.service;
 import com.example.board.entity.Post;
 import com.example.board.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -89,15 +90,22 @@ public class PostService {
     }
 
     public List<Post> searchPosts(String keyword) {
-//        return postRepository.findByTitleContaining(keyword);
-        return postRepository.searchByTitleNative(keyword);
+        return postRepository.findByTitleContaining(keyword);
     }
 
     public List<Post> searchPostsByTitleOrContent(String keyword) {
 //        return  postRepository.findByTitleContainingOrContentContaining(keyword, keyword);
-        return postRepository.searchByKeyword(keyword);
-
+//        return postRepository.searchByKeyword(keyword);
+        return postRepository.searchByTitleNative(keyword);
     }
 
+    public List<Post> getRecentPosts() {
+//        return postRepository.findTop3ByOrderByCreatedAtDesc();
+
+        return postRepository.findRecentPosts(PageRequest.of(0, 3));
+
+//        return postRepository.findRecentPostsNative();
+
+    }
 
 }
