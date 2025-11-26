214394363ed3ab19032ddace5ccd9f196fd4bdec
stanstanstanlee <stanstanstanlee@gmail.com>
2025-11-26 10:19:27 +0900

    comment read

--- src/main/java/com/example/board/controller/PostController.java
+++ src/main/java/com/example/board/controller/PostController.java
@@ -1,8 +1,11 @@
 package com.example.board.controller;
 
+import com.example.board.dto.CommentDTO;
 import com.example.board.dto.PostDTO;
+import com.example.board.entity.Comment;
 import com.example.board.entity.Post;
 import com.example.board.repository.PostRepository;
+import com.example.board.service.CommentService;
 import com.example.board.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -22,6 +25,7 @@ import java.util.List;
 public class PostController {
     private final PostService postService;
     private final PostRepository postRepository;
+    private final CommentService commentService;
 
 //    public PostController(PostRepository postRepository) { //롬복으로 자동생성
 //        this.postRepository = postRepository;
@@ -54,7 +58,12 @@ public class PostController {
     @GetMapping("/{id}")
     public String detail(@PathVariable Long id, Model model) {
         Post post = postService.getPostById(id);
+        List<Comment> comments = commentService.getCommentsByPostId(id);
+
+        model.addAttribute("comment", new CommentDTO());
         model.addAttribute("post", post);
+        model.addAttribute("comments", comments);
+
         return "posts/detail";
     }
 
@@ -153,6 +162,16 @@ public class PostController {
         Slice<Post> postSlice = postService.getPostsSlice(pageable);
         model.addAttribute("postSlice", postSlice);
         return "posts/list-more";
+    }
+
+    // Comment
+    @PostMapping("/{postId}/comments")
+    public String createComment(
+            @PathVariable Long postId,
+            @ModelAttribute Comment comment
+    ){
+        commentService.createComment(postId, comment);
+        return "redirect:/posts/" + postId;
     }
 
 }
--- src/main/java/com/example/board/service/CommentService.java
+++ src/main/java/com/example/board/service/CommentService.java
@@ -1,15 +1,34 @@
 package com.example.board.service;
 
+import com.example.board.dto.CommentDTO;
+import com.example.board.entity.Comment;
+import com.example.board.entity.Post;
 import com.example.board.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final PostService postService;
 
+    @Transactional
+    public Comment createComment(Long postId, Comment comment) {
+        Post post = postService.getPostById(postId);
+        comment.setPost(post);
+        return commentRepository.save(comment);
+
+    }
+
+    @Transactional
+    public List<Comment> getCommentsByPostId(Long postId) {
+        return commentRepository.findByPostId(postId);
+
+    }
 
 }
